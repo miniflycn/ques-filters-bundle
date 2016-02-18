ba78056c4391919f51842038378ea001dfca026b
Daniel Yang <miniflycn@justany.net>
2016-02-18 15:57:32 +0800

    update

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-var rollup = require('rollup'),
-	memory = require('rollup-plugin-memory'),
-	commonjs = require('rollup-plugin-commonjs'),
-	glob = require('glob');
+var rollup = require('rollup')
+  , memory = require('rollup-plugin-memory')
+  , commonjs = require('rollup-plugin-commonjs')
+  , glob = require('glob');
 
 /**
  * a-b-c => aBC
  * @private
  */
 function formatName(name) {
-	name = name.split('-');
-	if (name.length === 1) return name;
-	var res = name[0];
-	for (var i = 0, l = name.length; i < l; i++) {
-		res += name[1].replace(/^\w/, function (v) {
-			return v.toUpperCase();
-		});
-	}
-	return res;
+  name = name.split('-');
+  if (name.length === 1) return name;
+  var res = name[0];
+  for (var i = 0, l = name.length; i < l; i++) {
+    res += name[1].replace(/^\w/, function (v) {
+      return v.toUpperCase();
+    });
+  }
+  return res;
 }
 
 /**
@@ -25,39 +25,39 @@ function formatName(name) {
  * optoins.format {String} format type, default is 'cjs', could be 'amd', 'cjs', 'es6', 'iife', 'umd'
  */
 module.exports = function (options, cb) {
-	options.path = options.path || './components/';
-	options.format = options.format || 'cjs';
-	var path = options.path,
-		pathLength,
-		plugins = [],
-		input = '',
-		mains;
-	// fixed path
-	if (path[path.length - 1] !== '/') path += '/';
+  options.path = options.path || './components/';
+  options.format = options.format || 'cjs';
+  var path = options.path
+    , pathLength
+    , plugins = []
+    , input = ''
+    , mains;
+  // fixed path
+  if (path[path.length - 1] !== '/') path += '/';
 
-	pathLength = path.length;
-	mains = glob.sync(path + '**/main.js');
+  pathLength = path.length;
+  mains = glob.sync(path + '**/main.js');
 
-	for (var i = 0, l = mains.length; i < l; i++) {
-		input += 'import { filters as tmp' + i + ' } from "' + mains[i].slice(0, -3) + '";\n';
-		input += 'export var ' + formatName(mains[i].slice(pathLength, -8)) + ' = tmp' + i + ';\n';
-	}
+  for (var i = 0, l = mains.length; i < l; i++) {
+    input += 'import { filters as tmp' + i + ' } from "' + mains[i].slice(0, -3) + '";\n';
+    input += 'export var ' + formatName(mains[i].slice(pathLength, -8)) + ' = tmp' + i + ';\n';
+  }
 
-	plugins.push(memory({
-		contents: input
-	}), commonjs());
+  plugins.push(memory({
+    contents: input
+  }), commonjs());
 
-	rollup.rollup({
-		entry: 'main.js',
-		plugins: plugins
-	}).then(function (bundle) {
-		var result = bundle.generate({
-			format: options.format
-		});
+  rollup.rollup({
+    entry: 'main.js',
+    plugins: plugins
+  }).then(function (bundle) {
+    var result = bundle.generate({
+      format: options.format
+    });
 
-		cb(result.code);
-	}).catch(function (e) {
-		console.error(e);
-	});
+    cb(result.code);
+  }).catch(function (e) {
+    console.error(e);
+  });
 
 };
--- test/test.js
+++ test/test.js
@@ -1,18 +1,18 @@
-var bundle = require('../'),
-	vm = require('vm'),
-	_module = { exports: {}},
-	context = new vm.createContext({ module: _module, exports: _module.exports });
+var bundle = require('../')
+  , vm = require('vm')
+  , _module = { exports: {}}
+  , context = new vm.createContext({ module: _module, exports: _module.exports });
 
 describe('test', function () {
-	it('should build a bundle', function (done) {
-		bundle({ path: './test/components/' }, function (res) {
-			var script = new vm.Script(res);
-			script.runInContext(context);
-			_module.exports.a.hello('world').should.equal('world');
-			_module.exports.a.world(123).should.equal(124);
-			_module.exports.b.hello('world').should.equal('world');
-			_module.exports.b.world(123).should.equal(124);
-			done();
-		});
-	});
+  it('should build a bundle', function (done) {
+    bundle({ path: './test/components/' }, function (res) {
+      var script = new vm.Script(res);
+      script.runInContext(context);
+      _module.exports.a.hello('world').should.equal('world');
+      _module.exports.a.world(123).should.equal(124);
+      _module.exports.b.hello('world').should.equal('world');
+      _module.exports.b.world(123).should.equal(124);
+      done();
+    });
+  });
 });
